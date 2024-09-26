fd2bc480a39fe505199752aff7c47cd7dd98d31b
Victor Whyte <victordavidwhyte@gmail.com>
2024-09-26 23:58:34 +0100

    feat: add signin auth lgoic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -29,6 +29,11 @@ export class UsersController {
     return this.authService.signup(body.email, body.password);
   }
 
+  @Post('/signin')
+  signIn(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
+  }
+
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log('handler is runing');
