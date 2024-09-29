8b3899d95091408b563100940a4c19e13a22b297
Victor Whyte <victordavidwhyte@gmail.com>
2024-09-29 01:29:36 +0100

    feat: add auth guard

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+export class AuthGaurd implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session || {};
+    if (!userId) {
+      return false;
+    }
+    return true;
+  }
+}
