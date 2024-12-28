4577528d61b01f2a3c380cdb9125fe6e33189f34
Victor Whyte <victordavidwhyte@gmail.com>
2024-12-28 23:20:47 +0100

    feat: add sign in test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -26,7 +26,9 @@ describe('UsersController', () => {
       // update: () => {},
     };
     fakeAuthService = {
-      // signin: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
       // signup: () => {},
     };
     const module: TestingModule = await Test.createTestingModule({
@@ -64,5 +66,16 @@ describe('UsersController', () => {
   it('findUser throws an error if user with given id is not found', async () => {
     fakeUsersService.findOne = () => null;
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin updates session and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signIn(
+      { email: 'asdf@asdf.com', password: 'asdf' },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
